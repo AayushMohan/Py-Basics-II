4e054a3316f075cdf1f94cbf526dd4f5f8fd1ded
Aayush Mohan <66319691+AayushMohan@users.noreply.github.com>
2022-12-21 11:58:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# conditionals in python
+
+is_old = True
+is_licenced = True
+
+if is_old and is_licenced:
+  print('You are old enough to drive, and you have a licence!')
+
+else:
+  print('you are not of age!')
+
+print('okok')

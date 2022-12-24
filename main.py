056d9b1746cb4e84bad17302bb66f59fef7b42ab
Aayush Mohan <66319691+AayushMohan@users.noreply.github.com>
2022-12-24 08:49:22 +0000

    Completed Exercise on Logical Operators

--- main.py
+++ main.py
@@ -34,3 +34,23 @@
 ####### Logical Operators ########
 # < > == <= >= !=
 # and or not
+
+### EXERCISE ON LOGICAL OPERATOR ###
+
+is_magician = True
+is_expert = True
+
+#check if magician AND expert: "You are a master magician"
+
+#check if magician but not expert: "At least you are getting there"
+
+# if you're  not a magician: "You need magic powers"
+
+if is_magician and is_expert:
+  print("You are a master magician")
+
+elif is_magician and not is_expert:
+  print("At least you are getting there")
+
+elif not is_magician:
+  print("You need magic powers")

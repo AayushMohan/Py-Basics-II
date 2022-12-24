91820b1e7e6055f0b5be8f648734bbe090942c58
Aayush Mohan <66319691+AayushMohan@users.noreply.github.com>
2022-12-24 08:19:10 +0000

    Added Short Circuiting

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-# conditionals in python
+# # conditionals in python
 
-is_old = True
-is_licenced = True
+# is_old = True
+# is_licenced = True
 
-if is_old and is_licenced:
-  print('You are old enough to drive, and you have a licence!')
+# if is_old and is_licenced:
+#   print('You are old enough to drive, and you have a licence!')
 
-else:
-  print('you are not of age!')
+# else:
+#   print('you are not of age!')
 
-print('okok')
+# print('okok')
+
+# Ternary Operator
+
+#condition_if_true if condition else condition_if_else
+
+# is_friend = False
+
+# can_message = "message allowed" if is_friend else "not allowed to message"
+
+# Short Circuiting
+
+is_Friend = True
+
+is_User = False
+
+if is_Friend or is_User:
+  print('best friends forever')

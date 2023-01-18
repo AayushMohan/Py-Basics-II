fa4b75d43a06b0012e29a215f017b2f10d761699
Aayush Mohan <66319691+AayushMohan@users.noreply.github.com>
2023-01-18 11:56:03 +0000

    Added Args and Kwargs

--- main.py
+++ main.py
@@ -226,4 +226,14 @@
 #   return num % 2 == 0
 
 # print(is_even(51))
-  
+
+
+######## *args **kwargs #########
+
+# def super_func(*args, **kwargs):
+#   total=0
+#   for items in kwargs.values():
+#     total += items
+#   return sum(args) + total
+
+# print(super_func(1,2,3,4,5, num1=5, num2=10))

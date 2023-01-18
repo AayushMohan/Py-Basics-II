be2934e0993b004352fc7ade542a20950d903e23
Aayush Mohan <66319691+AayushMohan@users.noreply.github.com>
2023-01-18 09:42:35 +0000

    Added Duplicate checker

--- main.py
+++ main.py
@@ -143,3 +143,15 @@
 #     else:
 #       print(empty, end='')
 #   print('')
+
+# Exercise: Check for duplicates in list
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+duplicates = []
+
+for value in some_list:
+  if some_list.count(value) > 1:
+    if value not in duplicates:
+      duplicates.append(value)
+
+print(duplicates)

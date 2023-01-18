2f999f6b16cdb818b26e0db4fcb31728fb8e07fa
Aayush Mohan <66319691+AayushMohan@users.noreply.github.com>
2023-01-18 09:12:42 +0000

    Added While Loop

--- main.py
+++ main.py
@@ -113,3 +113,18 @@
 #   i += 1
 # else:
 #   print("Done with all the work")
+
+# my_list = [1, 2, 3]
+# for item in my_list:
+#   print(item)
+
+# i = 0
+
+# while i < len(my_list):
+#   print(my_list)
+#   i += 1
+
+# while True:
+#   response = input('Say Something: ')
+#   if (response == 'bye'):
+#     break

fe26437deb852a38aa35509bc4c6c3605d172122
Aayush Mohan <66319691+AayushMohan@users.noreply.github.com>
2023-01-18 15:46:14 +0000

    Added Scope Rules

--- main.py
+++ main.py
@@ -261,9 +261,26 @@
 ######## SCOPE #######
 # Scope - what variables do I have access to?
 
-if True:
-  x = 10
+# if True:
+#   x = 10
 
-def some_func():
-  total = 100
-  print(x)
+# def some_func():
+#   total = 100
+#   print(x)
+
+####### SCOPE RULES #########
+
+a = 1
+
+def parent():
+  def confusion():
+    return sum
+  return confusion()
+
+print(parent())
+print(a)
+    
+#1 - start with local
+#2 - Parent local?
+#3 - Global
+#4 - built in python functions.

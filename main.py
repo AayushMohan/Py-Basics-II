3782bdc19aea262d44578a6932bac4602a8b62de
Aayush Mohan <66319691+AayushMohan@users.noreply.github.com>
2023-01-18 09:31:19 +0000

    Added GUI

--- main.py
+++ main.py
@@ -128,3 +128,18 @@
 #   response = input('Say Something: ')
 #   if (response == 'bye'):
 #     break
+
+# ######### GUI Exercise ########
+# picture = [[0, 0, 0, 1, 0, 0, 0], [0, 0, 1, 1, 1, 0, 0], [0, 1, 1, 1, 1, 1, 0],
+#            [1, 1, 1, 1, 1, 1, 1], [0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0]]
+
+# fill = '*'
+# empty = ' '
+
+# for image in picture:
+#   for pixel in image:
+#     if (pixel):
+#       print(fill, end='')
+#     else:
+#       print(empty, end='')
+#   print('')
